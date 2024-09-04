05c7eb8ce06866ffd341b53fabc0d4397ce5df89
Matthew Pownall <K032498@eltham-college.org.uk>
2024-09-04 12:55:53 +0100

    getting there

--- Board.cs
+++ Board.cs
@@ -13,17 +13,19 @@ namespace Coursework_UI
     internal class Board 
     {
         private Counter[] Grid;
-        private Counter Player;
+        private Counter[,] Grid2D;
+        private Counter CurrentPlayer;
 
 
         public Board()
         {
             Grid = new Counter[42];
+            Grid2D = new Counter[7, 6];
             for (int x = 0; x < Grid.Length; x++)
             {
                 Grid[x] = new Counter("0");
             }
-            Player = new Counter("1");
+            CurrentPlayer = new Counter("1");
 
 
 
@@ -35,58 +37,54 @@ namespace Coursework_UI
 
 
 
-
-        public bool CheckWin()
-        {
-            bool Win = false;
-
-
-
-
-            return Win;
-        }
-
-
-
         public void PlaceCounter(int C, string v)
         {
             int pos = C * 6;
+            int y = 0;
+            bool win = false;
             while (pos < C*6 + 6)
                 if (Grid[pos].Colour == "0")
                 {
                     Grid[pos].Colour = v;
+                    Grid2D[C, y] = new Counter(v);
+                    win = checkWin(C, y);
                     break;
                 }
                 else
                 {
                     pos++;
+                    y++;
                 }
+            
             UpdatePlayer();
 
         }
         public Counter u
         {
-            get { return Player; }
+            get { return CurrentPlayer; }
         }
 
         private void UpdatePlayer()
         {
-            if (Player.Colour == "1") Player.Colour = "2";
-            else Player.Colour = "1";
+            if (CurrentPlayer.Colour == "1") CurrentPlayer.Colour = "2";
+            else CurrentPlayer.Colour = "1";
         }
 
 
 
-        public bool CheckWin(int l)
+        private bool checkWin(int x, int y, string Player)
         {
             bool win = false;
+            int count = 0;
 
+            //Vertical Check
 
+            for(int j = 0; j < 7; j++)
+            {
+                if (Grid2D[x, j] == player)
+            }
 
-
-
-           return win;
-
+            return win;
         }
 
 
