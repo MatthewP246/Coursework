81ac2b9b9dd2bba8d17df2dc0d4b9a9fa8150bf0
Matthew Pownall <K032498@eltham-college.org.uk>
2024-09-09 15:49:49 +0100

    checkwin getting there

--- Board.cs
+++ Board.cs
@@ -13,14 +13,14 @@ namespace Coursework_UI
     internal class Board 
     {
         private Counter[] Grid;
-        private Counter[,] Grid2D;
+
         private Counter CurrentPlayer;
 
 
         public Board()
         {
             Grid = new Counter[42];
-            Grid2D = new Counter[7, 6];
+
             for (int x = 0; x < Grid.Length; x++)
             {
                 Grid[x] = new Counter("0");
@@ -40,23 +40,24 @@ namespace Coursework_UI
         public void PlaceCounter(int C, string v)
         {
             int pos = C * 6;
-            int y = 0;
             bool win = false;
             while (pos < C*6 + 6)
                 if (Grid[pos].Colour == "0")
                 {
                     Grid[pos].Colour = v;
-                    Grid2D[C, y] = new Counter(v);
-                    win = checkWin(C, y);
+                    win = checkWin(C, v);
                     break;
                 }
                 else
                 {
                     pos++;
-                    y++;
                 }
             
             UpdatePlayer();
+            if(win == true)
+            {
+                CurrentPlayer.Colour = "0";
+            }
 
         }
         public Counter u
@@ -72,18 +73,62 @@ namespace Coursework_UI
 
 
 
-        private bool checkWin(int x, int y, string Player)
+        private bool checkWin(int x, string Player)
         {
             bool win = false;
             int count = 0;
+            int pos = x * 6;
 
             //Vertical Check
 
-            for(int j = 0; j < 7; j++)
+            while(pos<x*6+6)
             {
-                if (Grid2D[x, j] == player)
+                if(Grid[pos].Colour != "0")
+                {
+                    if (Grid[pos].Colour == Player) count++;
+                    else
+                    {
+                        count = 0;
+                    }  
+                }
+                pos++;
+
+            }
+            if(count >= 4)
+            {
+                win = true;
             }
 
+            //Horizontal Check
+
+            if (win == false)
+            {
+                count = 0;
+                for (int i = 0; i < 8; i++)
+                {
+                    for (int j = i; j < 42; j = j + 6)
+                    {
+                        if (Grid[j].Colour != "0")
+                        {
+                            if (Grid[j].Colour == Player) count++;
+                            else
+                            {
+                                count = 0;
+                            }
+
+                        }
+                        if (count >= 4)
+                        {
+                            win = true;
+                            break;
+                        }
+                    }
+                }
+            }
+            
+            
+            
+            
             return win;
         }
 
--- Grid.xaml.cs
+++ Grid.xaml.cs
@@ -19,9 +19,107 @@ namespace Coursework_UI
     /// </summary>
     public partial class Grid : Window
     {
+        private Connect4 Game;
+        public int PlayerCount = 0;
         public Grid()
         {
             InitializeComponent();
+            Game = new Connect4();
+            DataContext = Game;
+        }
+
+        private void Column1_Click(object sender, RoutedEventArgs e)
+        {
+            if (PlayerCount % 2 == 0)
+            {
+                Game.PlaceCounter(0, "1");
+            }
+            else
+            {
+                Game.PlaceCounter(0, "2");
+            }
+
+            PlayerCount++;
+        }
+        private void Column2_Click(object sender, RoutedEventArgs e)
+        {
+            if (PlayerCount % 2 == 0)
+            {
+                Game.PlaceCounter(1, "1");
+            }
+            else
+            {
+                Game.PlaceCounter(1, "2");
+            }
+
+            PlayerCount++;
+        }
+
+        private void Column3_Click(object sender, RoutedEventArgs e)
+        {
+            if (PlayerCount % 2 == 0)
+            {
+                Game.PlaceCounter(2, "1");
+            }
+            else
+            {
+                Game.PlaceCounter(2, "2");
+            }
+
+            PlayerCount++;
+        }
+
+        private void Column4_Click(object sender, RoutedEventArgs e)
+        {
+            if (PlayerCount % 2 == 0)
+            {
+                Game.PlaceCounter(3, "1");
+            }
+            else
+            {
+                Game.PlaceCounter(3, "2");
+            }
+            PlayerCount++;
+        }
+
+        private void Column5_Click(object sender, RoutedEventArgs e)
+        {
+            if (PlayerCount % 2 == 0)
+            {
+                Game.PlaceCounter(4, "1");
+            }
+            else
+            {
+                Game.PlaceCounter(4, "2");
+            }
+
+            PlayerCount++;
+        }
+
+        private void Column6_Click(object sender, RoutedEventArgs e)
+        {
+            if (PlayerCount % 2 == 0)
+            {
+                Game.PlaceCounter(5, "1");
+            }
+            else
+            {
+                Game.PlaceCounter(5, "2");
+            }
+            PlayerCount++;
+        }
+
+        private void Column7_Click(object sender, RoutedEventArgs e)
+        {
+            if (PlayerCount % 2 == 0)
+            {
+                Game.PlaceCounter(6, "1");
+            }
+            else
+            {
+                Game.PlaceCounter(6, "2");
+            }
+            PlayerCount++;
         }
     }
 }
