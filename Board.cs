e060892d45d9fe72b0ca056db94c6c96c7f3baff
Matthew Pownall <K032498@eltham-college.org.uk>
2024-11-29 15:15:07 +0000

    checkwin has imnproved

--- Board.cs
+++ Board.cs
@@ -63,9 +63,8 @@ namespace Coursework_UI
         public void PlaceCounter(int C)
         {
             bool win = false;
-            int pos = C * 6;
 
-            if (RecursivePlace(C, pos) == true)
+            if (RecursivePlace(C) == true)
             { 
                 win = checkWin();
 
@@ -73,26 +72,27 @@ namespace Coursework_UI
                 if (win == true)
                 {
                     CurrentPlayer.Colour = "0";
+                    
                 }
             }
         }
 
 
-        private bool RecursivePlace(int C, int pos)
+        private bool RecursivePlace(int C)
         {
             //Recursive algorithm to place counter
             bool Placed = true;
-            if (pos < C * 6 + 6)
+            if (C<42)
             {
 
-                if (g[pos].Colour == "0")
+                if (g[C].Colour == "0")
                 {
-                    g[pos].Colour = CurrentPlayer.Colour;
-                    gg[C, pos % 6].Colour = CurrentPlayer.Colour;
+                    g[C].Colour = CurrentPlayer.Colour;
+                    gg[C%7,C/7].Colour = CurrentPlayer.Colour;
                 }
                 else 
                 {
-                    Placed = RecursivePlace(C, pos + 1);
+                    Placed = RecursivePlace(C+7);
 
                 }
                 
@@ -119,23 +119,71 @@ namespace Coursework_UI
         {
 
             bool win = false;
+            ulong[] Rows = new ulong[6];
             ulong Bitboard = 0;
 
-            for (int a = 0; a < 42; a++)
+            //Creates a bitboard for each row in the grid
+            for (int x = 0; x < 7; x++)
             {
-                if (Grid[a].Colour == CurrentPlayer.Colour)
+                for(int y = 0; y < 6; y++)
                 {
-                    Bitboard = Bitboard + Convert.ToUInt64(Math.Pow(2, a));
+                    if (Grid[x+y*7].Colour == CurrentPlayer.Colour)
+                    {
+                        Rows[y] = Rows[y] + Convert.ToUInt64(Math.Pow(2, 6-x));
+                    }
                 }
-
             }
 
 
+
+            for (int i = 0; i < 6; i++)
+            {
+                //Horizontal Check:
+                ulong Horizontal = Rows[i] & (Rows[i] << 1) & (Rows[i] << 2) & (Rows[i] << 3);
+
+
+                //Prevents index out of bounds errors
+                ulong Vertical = 0;
+                ulong Diagonal1 = 0;
+                ulong Diagonal2 = 0;
+                if (i < 3)
+                {
+                    //Vertical Check:
+
+                    Vertical = Rows[i] & Rows[i + 1] & Rows[i + 2] & Rows[i + 3];
+
+                    //Diagonal Check (bottom-left to top-right)
+                    Diagonal1 = Rows[i] & (Rows[i + 1] << (i + 1)) & (Rows[i + 2] << (i + 2)) & (Rows[i + 3] << (i + 3));
+
+                    //Diagonal Check (bottom-right to top-left)
+                    Diagonal2 = Rows[i] & (Rows[i + 1] >> (i + 1)) & (Rows[i + 2] >> (i + 2)) & (Rows[i + 3] >> (i + 3));
+                }
+                // Return true if any of the checks are non-zero, indicating a win
+                if(Horizontal != 0 || Vertical != 0 || Diagonal1 != 0 || Diagonal2 != 0)
+                {
+                    win = true;
+                    break;
+                }
+            }
+
+            
+
+            return win;
+        }
+
+
+        private void something()
+        {
+            bool win;
+            ulong Bitboard = 0;
+            Console.WriteLine(Bitboard);
+
+
             // Horizontal check: Shift by 1, 2, and 3 bits
-            ulong horizontal = Bitboard & (Bitboard >> 1) & (Bitboard >> 2) & (Bitboard >> 3);
+            ulong vertical = Bitboard & (Bitboard >> 1) & (Bitboard >> 2) & (Bitboard >> 3);
 
             // Vertical check: Shift by 7, 14, and 21 bits (7 bits per row
-            ulong vertical = Bitboard & (Bitboard >> 7) & (Bitboard >> 14) & (Bitboard >> 21);
+            ulong horizontal = Bitboard & (Bitboard >> 7) & (Bitboard >> 14) & (Bitboard >> 21);
 
             // Diagonal check (bottom-right to top-left): Shift by 6, 12, and 18 
             ulong diagonal1 = Bitboard & (Bitboard >> 6) & (Bitboard >> 12) & (Bitboard >> 18);
@@ -145,9 +193,6 @@ namespace Coursework_UI
 
             // Return true if any of the checks are non-zero, indicating a win
             win = (horizontal != 0 || vertical != 0 || diagonal1 != 0 || diagonal2 != 0);
-
-            return win;
-
 
         }
     }
